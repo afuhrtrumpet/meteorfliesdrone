5d4a773a57de19decaec36523b73ffddbdb042b8
Alex Fuhr <afuhrtrumpet>
2014-07-26 14:00:43 -0700

    Added beginnings of button template

---
+++ client/client.js
@@ -0,0 +1,26 @@
+Template.buttons.commands = [
+{
+	name: "Forward",
+	img: "forward.png"
+},
+{
+	name: "Left",
+	img: "left.png"
+},
+{
+	name: "Back",
+	img: "back.png"
+},
+{
+	name: "Right",
+	img: "right.png"
+},
+{
+	name: "Clockwise",
+	img: "clockwise.png"
+},
+{
+	name: "Counterclockwise",
+	img: "counterclockwise.png"
+}
+];
