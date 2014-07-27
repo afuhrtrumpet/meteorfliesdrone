0a28e3d6503c58e1cd1a04ee8ca38fcf3ecf0ca7
anjosh <joshuaan1@gmail.com>
2014-07-26 21:55:53 -0700

    Got rid of using emails, using usernames instead. Made admin template only visible to "admin" user

--- client/client.js
+++ client/client.js
@@ -90,3 +90,12 @@ Template.adminPanel.events({
 Template.main.userLoggedIn = function () {
 		return 1 || Meteor.userId();
 };
+
+Template.main.isAdmin = function () {
+	var user = Meteor.user();
+	return ( user && user.username === 'admin' );
+}
+
+Accounts.ui.config({
+	passwordSignupFields: 'USERNAME_ONLY'
+});
